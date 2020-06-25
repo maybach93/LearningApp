1e9aa69a0161d835032f34b7875fece9fc85a0a1
Vitalii <vitali.poponov@waveaccess.ru>
2020-06-25 21:45:21 +0300

    Initial Commit

---
+++ Learning/LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp.swift
+//  Learning
+//
+//  Created by Vitalii Poponov on 6/25/20.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
