5f05f83b1edbd03e861b6aa75d6ba924805a9d73
Vitalii <vitali.poponov@waveaccess.ru>
2020-07-04 01:46:46 +0300

    refactoring and animation fix

--- Learning/Modules/Main/MainView.swift
+++ Learning/Modules/Main/MainView.swift
@@ -7,79 +7,93 @@
 
 import SwiftUI
 
+struct MainMenusView: View {
+    
+    var summaryWidgetView: SummaryWidgetView
+    
+    var body: some View {
+        List {
+
+            summaryWidgetView.listRowBackground(Color.clear).padding([.top, .bottom], 10)
+
+            Button {
+            } label: {
+                ZStack {
+                    HStack {
+                        Image(systemName: "pencil.circle.fill").resizable().frame(width: 25, height: 25, alignment: .center).foregroundColor(Color.blue)
+                        Text("My profile").font(.body).fontWeight(.medium).frame(alignment: .leading)
+                        Spacer()
+                    }
+                }
+            }.listRowBackground(Color.clear)
+
+            Button {
+
+            } label: {
+                ZStack {
+                    HStack {
+                        Image(systemName: "books.vertical.fill").resizable().frame(width: 25, height: 22, alignment: .center).foregroundColor(Color(.brightYellow))
+                        Text("Vocabulary").font(.body).fontWeight(.medium).frame(alignment: .leading)
+                        Spacer()
+                    }
+                }
+            }.listRowBackground(Color.clear)
+            Button {
+            } label: {
+                ZStack {
+                    HStack {
+                        Image(systemName: "archivebox.circle.fill").resizable().frame(width: 25, height: 25, alignment: .center).foregroundColor(Color.green)
+                        Text("Grammar").font(.body).fontWeight(.medium).frame(alignment: .leading)
+                        Spacer()
+                    }
+                }
+            }.listRowBackground(Color.clear)
+            Button {
+
+            } label: {
+                ZStack {
+                    HStack {
+                        Image(systemName: "archivebox.circle").resizable().frame(width: 25, height: 25, alignment: .center).foregroundColor(Color.red)
+                        Text("Recent practice").font(.body).fontWeight(.medium).frame(alignment: .leading)
+                        Spacer()
+                    }
+
+                }
+            }
+            .listRowBackground(Color.clear)
+            Button {
+            } label: {
+
+                    HStack {
+                        Image(systemName: "gearshape.fill").resizable().frame(width: 25, height: 25, alignment: .center).foregroundColor(Color.orange)
+                        Text("Settings").font(.body).fontWeight(.medium).frame(alignment: .leading)
+                        Spacer()
+                    }
+
+
+            }.listRowBackground(Color.clear)
+        }.listRowInsets(EdgeInsets(top: 40, leading: 0, bottom: 40, trailing: 20)).animation(.none)
+    }
+}
+
 struct MainView: View {
+    
     @State var isSpeakPressed: Bool = false
     @ObservedObject var viewModel: MainViewModel
+    
+    var summaryWidgetView: SummaryWidgetView?
+    
     init(viewModel: MainViewModel) {
         self.viewModel = viewModel
+        self.summaryWidgetView = SummaryWidgetView(viewModel: SummaryWidgetViewModel(router: viewModel.router))
     }
     
     var body: some View {
-        return
-            ZStack {
-                Color(.main).edgesIgnoringSafeArea(.all)
-                List {
-         
-                    SummaryWidgetView(viewModel: SummaryWidgetViewModel(router: viewModel.router)).listRowBackground(Color.clear).padding([.top, .bottom], 10)
-                   
-                    Button {
-                    } label: {
-                        ZStack {
-                            HStack {
-                                Image(systemName: "pencil.circle.fill").resizable().frame(width: 25, height: 25, alignment: .center).foregroundColor(Color.blue)
-                                Text("My profile").font(.body).fontWeight(.medium).frame(alignment: .leading)
-                                Spacer()
-                            }
-                        }
-                    }.listRowBackground(Color.clear)
-
-                    Button {
-                        
-                    } label: {
-                        ZStack {
-                            HStack {
-                                Image(systemName: "books.vertical.fill").resizable().frame(width: 25, height: 22, alignment: .center).foregroundColor(Color(.brightYellow))
-                                Text("Vocabulary").font(.body).fontWeight(.medium).frame(alignment: .leading)
-                                Spacer()
-                            }
-                        }
-                    }.listRowBackground(Color.clear)
-                    Button {
-                    } label: {
-                        ZStack {
-                            HStack {
-                                Image(systemName: "archivebox.circle.fill").resizable().frame(width: 25, height: 25, alignment: .center).foregroundColor(Color.green)
-                                Text("Grammar").font(.body).fontWeight(.medium).frame(alignment: .leading)
-                                Spacer()
-                            }
-                        }
-                    }.listRowBackground(Color.clear)
-                    Button {
-                        
-                    } label: {
-                        ZStack {
-                            HStack {
-                                Image(systemName: "archivebox.circle").resizable().frame(width: 25, height: 25, alignment: .center).foregroundColor(Color.red)
-                                Text("Recent practice").font(.body).fontWeight(.medium).frame(alignment: .leading)
-                                Spacer()
-                            }
-
-                        }
-                    }
-                    .listRowBackground(Color.clear)
-                    Button {
-                    } label: {
-                        
-                            HStack {
-                                Image(systemName: "gearshape.fill").resizable().frame(width: 25, height: 25, alignment: .center).foregroundColor(Color.orange)
-                                Text("Settings").font(.body).fontWeight(.medium).frame(alignment: .leading)
-                                Spacer()
-                            }
-
-                        
-                    }.listRowBackground(Color.clear)
-                }.listRowInsets(EdgeInsets(top: 40, leading: 0, bottom: 40, trailing: 20)).animation(.none)
-                
+        return ZStack {
+            Color(.main).edgesIgnoringSafeArea(.all)
+            if let summaryWidgetView = self.summaryWidgetView {
+                MainMenusView(summaryWidgetView: summaryWidgetView)
+            }
             VStack {
                 Spacer()
                     Button("", action: {
@@ -92,7 +106,6 @@ struct MainView: View {
                     ConversationView(viewModel: ConversationViewModel(router: viewModel.router)).transition(.opacity).edgesIgnoringSafeArea(.all)
                 }
             }
-            
             .onReceive(self.viewModel.router.$firstController) { (output) in
                 if case .main = output {
                     withAnimation {
@@ -100,5 +113,6 @@ struct MainView: View {
                     }
                 }
             }
+            
     }
 }
